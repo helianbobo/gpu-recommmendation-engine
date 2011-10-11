63ba4d6439884f74a3e391c43f4f721f4c3eb66f
Liu Chao <liuchao36@gmail.com>
2011-10-11 17:57:42 +0800

    Init

---
+++ src/main/java/Dummy.java
@@ -0,0 +1,7 @@
+/**
+ * User: cliu3
+ * Date: 11-10-11
+ * Time: 下午5:56
+ */
+public class Dummy {
+}
---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,7 @@
+/**
+ * User: cliu3
+ * Date: 11-10-11
+ * Time: 下午5:57
+ */
+public class DummyTest {
+}
