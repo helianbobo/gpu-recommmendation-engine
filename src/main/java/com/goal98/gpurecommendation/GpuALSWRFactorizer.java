bbfa33b8b00678e80cae7e16f32ece23bf9f662f
helianbobo <liuchao36@gmail.com>
2011-10-14 00:06:59 +0800

    Aparapi

--- src/main/java/com/goal98/gpurecommendation/GpuALSWRFactorizer.java
+++ src/main/java/com/goal98/gpurecommendation/GpuALSWRFactorizer.java
@@ -1,6 +1,7 @@
 package com.goal98.gpurecommendation;
 
 
+import com.amd.aparapi.Kernel;
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.common.FullRunningAverage;
 import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
@@ -17,6 +18,7 @@ import org.apache.mahout.math.Vector;
 import org.apache.mahout.math.als.AlternateLeastSquaresSolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.rmi.runtime.Log;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,14 +37,6 @@ public class GpuALSWRFactorizer extends ALSWRFactorizer {
 
     private final int numIterations;
 
-    public DataModel getDataModel() {
-        return dataModel;
-    }
-
-    public int getNumFeatures() {
-        return numFeatures;
-    }
-
     private static final Logger log = LoggerFactory.getLogger(ALSWRFactorizer.class);
 
     public GpuALSWRFactorizer(DataModel dataModel, int numFeatures, double lambda, int numIterations) throws TasteException {
@@ -54,118 +48,154 @@ public class GpuALSWRFactorizer extends ALSWRFactorizer {
 
     }
 
+    static class ALSWRKernel extends Kernel {
+
+        double[] features;
+        int numFeatures;
+
+        ALSWRKernel(double[] features, int numFeatures) {
+            this.features = features;
+            this.numFeatures = numFeatures;
+        }
+
+        @Override
+        public void run() {
+            int globalId = getGlobalId();
+            final int index = globalId * numFeatures;
+            features[index] = features[index] * 2;
+        }
+    }
+
     @Override
     public Factorization factorize() throws TasteException {
         log.info("starting to compute the factorization...");
         final AlternateLeastSquaresSolver solver = new AlternateLeastSquaresSolver();
-        final Features features = new Features(this);
 
-        for (int iteration = 0; iteration < numIterations; iteration++) {
-            log.info("iteration {}", iteration);
+        final double[] featuresM;
+        final double[] featuresU;
 
-            /* fix M - compute U */
-            ExecutorService queue = createQueue();
-            LongPrimitiveIterator userIDsIterator = dataModel.getUserIDs();
-            try {
-                while (userIDsIterator.hasNext()) {
-                    final long userID = userIDsIterator.nextLong();
-                    final LongPrimitiveIterator itemIDsFromUser = dataModel.getItemIDsFromUser(userID).iterator();
-                    final PreferenceArray userPrefs = dataModel.getPreferencesFromUser(userID);
-                    queue.execute(new Runnable() {
-                        @Override
-                        public void run() {
-                            List<Vector> featureVectors = new ArrayList<Vector>();
-                            while (itemIDsFromUser.hasNext()) {
-                                long itemID = itemIDsFromUser.nextLong();
-                                featureVectors.add(features.getItemFeatureColumn(itemIndex(itemID)));
-                            }
-                            Vector userFeatures = solver.solve(featureVectors, ratingVector(userPrefs), lambda, numFeatures);
-                            features.setFeatureColumnInU(userIndex(userID), userFeatures);
-                        }
-                    });
-                }
-            } finally {
-                queue.shutdown();
-                try {
-                    queue.awaitTermination(dataModel.getNumUsers(), TimeUnit.SECONDS);
-                } catch (InterruptedException e) {
-                    throw new IllegalStateException("Error when computing user features", e);
-                }
-            }
-
-            /* fix U - compute M */
-            queue = createQueue();
-            LongPrimitiveIterator itemIDsIterator = dataModel.getItemIDs();
-            try {
-                while (itemIDsIterator.hasNext()) {
-                    final long itemID = itemIDsIterator.nextLong();
-                    final PreferenceArray itemPrefs = dataModel.getPreferencesForItem(itemID);
-                    queue.execute(new Runnable() {
-                        @Override
-                        public void run() {
-                            List<Vector> featureVectors = new ArrayList<Vector>();
-                            for (Preference pref : itemPrefs) {
-                                long userID = pref.getUserID();
-                                featureVectors.add(features.getUserFeatureColumn(userIndex(userID)));
-                            }
-                            Vector itemFeatures = solver.solve(featureVectors, ratingVector(itemPrefs), lambda, numFeatures);
-                            features.setFeatureColumnInM(itemIndex(itemID), itemFeatures);
-                        }
-                    });
-                }
-            } finally {
-                queue.shutdown();
-                try {
-                    queue.awaitTermination(dataModel.getNumItems(), TimeUnit.SECONDS);
-                } catch (InterruptedException e) {
-                    throw new IllegalStateException("Error when computing item features", e);
-                }
+        log.info("features initialization ...");
+        Random random = RandomUtils.getRandom();
+        featuresM = new double[this.dataModel.getNumItems() * this.numFeatures];
+        LongPrimitiveIterator itemIDsIterator = this.dataModel.getItemIDs();
+        while (itemIDsIterator.hasNext()) {
+            long itemID = itemIDsIterator.nextLong();
+            int itemIDIndex = itemIndex(itemID);
+            featuresM[itemIDIndex * numFeatures] = averateRating(itemID);
+            for (int feature = 1; feature < this.numFeatures; feature++) {
+                featuresM[itemIDIndex * numFeatures + feature] = random.nextDouble() * 0.1;
             }
         }
+        featuresU = new double[this.dataModel.getNumUsers() * this.numFeatures];
+
+
+        log.info("start kernelM...");
+        Kernel kernelM = new ALSWRKernel(featuresM, numFeatures);
+//        kernelM.setExecutionMode(Kernel.EXECUTION_MODE.JTP);
+        final int numItems = dataModel.getNumItems();
+        kernelM.execute(1024);
+        kernelM.dispose();
+
+
+
+        log.info("start kernelU...");
+        Kernel kernelU = new ALSWRKernel(featuresU, numFeatures);
+//        kernelU.setExecutionMode(Kernel.EXECUTION_MODE.JTP);
+        final int numUsers = dataModel.getNumUsers();
+        kernelU.execute(1024);
+        kernelU.dispose();
+
 
         log.info("finished computation of the factorization...");
-        return createFactorization(features.getU(), features.getM());
+
+        double[][] M = new double[numItems][numFeatures];
+        double[][] U = new double[numItems][numFeatures];
+
+        buildMatrix(U, featuresU);
+        buildMatrix(M, featuresM);
+        return createFactorization(U, M);
     }
+
+    private void buildMatrix(double[][] u, double[] featuresU) {
+        for (int i = 0; i < featuresU.length; i++) {
+            double featureValue = featuresU[i];
+            u[i / numFeatures][i % numFeatures] = featureValue;
+
+        }
+    }
+
+    private double averateRating(long itemID) throws TasteException {
+        PreferenceArray prefs = dataModel.getPreferencesForItem(itemID);
+        RunningAverage avg = new FullRunningAverage();
+        for (Preference pref : prefs) {
+            avg.addDatum(pref.getValue());
+        }
+        return avg.getAverage();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     static class Features {
 
         private final DataModel dataModel;
         private final int numFeatures;
 
-        private final double[][] M;
-        private final double[][] U;
+        private final double[] M;
+        private final double[] U;
 
         Features(GpuALSWRFactorizer factorizer) throws TasteException {
             this.dataModel = factorizer.dataModel;
             this.numFeatures = factorizer.numFeatures;
             Random random = RandomUtils.getRandom();
-            M = new double[this.dataModel.getNumItems()][this.numFeatures];
+            M = new double[this.dataModel.getNumItems() * this.numFeatures];
             LongPrimitiveIterator itemIDsIterator = this.dataModel.getItemIDs();
             while (itemIDsIterator.hasNext()) {
                 long itemID = itemIDsIterator.nextLong();
                 int itemIDIndex = factorizer.itemIndex(itemID);
-                M[itemIDIndex][0] = averateRating(itemID);
+                M[itemIDIndex * numFeatures] = averateRating(itemID);
                 for (int feature = 1; feature < this.numFeatures; feature++) {
-                    M[itemIDIndex][feature] = random.nextDouble() * 0.1;
+                    M[itemIDIndex * numFeatures + feature] = random.nextDouble() * 0.1;
                 }
             }
-            U = new double[this.dataModel.getNumUsers()][this.numFeatures];
+            U = new double[this.dataModel.getNumUsers() * this.numFeatures];
         }
 
-        double[][] getM() {
+        double[] getM() {
             return M;
         }
 
-        double[][] getU() {
+        double[] getU() {
             return U;
         }
 
         DenseVector getUserFeatureColumn(int index) {
-            return new DenseVector(U[index]);
+            double[] values = new double[numFeatures];
+            for (int i = 0; i < numFeatures; i++) {
+                values[i] = U[index * numFeatures + i];
+
+            }
+            return new DenseVector(values);
         }
 
         DenseVector getItemFeatureColumn(int index) {
-            return new DenseVector(M[index]);
+            double[] values = new double[numFeatures];
+            for (int i = 0; i < numFeatures; i++) {
+                values[i] = M[index * numFeatures + i];
+
+            }
+            return new DenseVector(values);
         }
 
         void setFeatureColumnInU(int idIndex, Vector vector) {
@@ -176,9 +206,9 @@ public class GpuALSWRFactorizer extends ALSWRFactorizer {
             setFeatureColumn(M, idIndex, vector);
         }
 
-        protected void setFeatureColumn(double[][] matrix, int idIndex, Vector vector) {
+        protected void setFeatureColumn(double[] matrix, int idIndex, Vector vector) {
             for (int feature = 0; feature < numFeatures; feature++) {
-                matrix[idIndex][feature] = vector.get(feature);
+                matrix[idIndex * numFeatures + feature] = vector.get(feature);
             }
         }
 
